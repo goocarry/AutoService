4f8e2873751a5b78cef8f69330234cffe878b217
goocarry <goocarry@gmail.com>
2019-05-21 15:50:59 +0900

    list of repairs

--- src/screens/ServiceCardScreen.js
+++ src/screens/ServiceCardScreen.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
 import { brands } from '../data/brand_db';
 import firebase from 'react-native-firebase';
+import ItemComponent from '../components/ItemComponent';
 
 
 export default class ServiceCardScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      items: []
     }
   }
 
@@ -15,18 +17,51 @@ export default class ServiceCardScreen extends Component {
     title: 'Сервисная карта авто'
   }
 
-  goto = () => {
-    
+
+  componentDidMount() {
+    this.readUserData();
   }
 
+  /*
+  readUserData() {
+    firebase.database().ref('repairList/').once('value', function (snapshot) {
+      //console.log(snapshot.val())
+      //alert(JSON.stringify(snapshot.val()))
+    }).then(snapshot => this.setState({ repairList: JSON.stringify(snapshot.val())}));
+  }
+  */
+
+  readUserData() {
+    firebase.database().ref('repairList/').on('value', snapshot => {
+      //console.log(snapshot.val())
+      //alert(JSON.stringify(snapshot.val()))
+      let data = snapshot.val();
+      let items = Object.values(data);
+      this.setState({ items });
+    })
+  }
+
+  check = () => {
+    alert(this.state.items)
+  }
 
   render() {
     return (
-      <View style={styles.container}>
+      <View style={styles.container} >
         <Text>ServiceCardScreen</Text>
         <TouchableOpacity onPress={() => this.props.navigation.navigate('RepairAdding')}>
           <Text style={styles.btnText}>Добавить новую запись</Text>
         </TouchableOpacity>
+        <TouchableOpacity onPress={this.check}>
+          <Text style={styles.btnText}>check</Text>
+        </TouchableOpacity>
+        <View style={styles.container}>
+          {this.state.items.length > 0 ? (
+            <ItemComponent items={this.state.items} />
+          ) : (
+              <Text>No items</Text>
+            )}
+        </View>
       </View>
     );
   }
