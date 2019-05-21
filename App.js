de870cf41bd313563bf5e325ddf52041141cf0e8
goocarry <goocarry@gmail.com>
2019-05-21 23:17:43 +0900

    icons on tab navigator

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { createSwitchNavigator, createStackNavigator, createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 import LoginScreen from './src/screens/LoginScreen';
 import HomeScreen from './src/screens/HomeScreen';
 import AuthLoadingScreen from './src/screens/AuthLoadingScreen';
@@ -41,7 +43,32 @@ const AppStack = createStackNavigator(
           ChatTab: { screen: ChatScreen },
           ProfileTab: { screen: ProfileScreen },
           PriceHelperTab: { screen: PriceHelperScreen }
-        }, {
+        },
+        {
+          defaultNavigationOptions: ({ navigation }) => ({
+            tabBarIcon: ({ focused, horizontal, tintColor }) => {
+              const { routeName } = navigation.state;
+              let IconComponent = Ionicons;
+              let iconName;
+              if (routeName === 'HomeTab') {
+                iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+                // Sometimes we want to add badges to some icons. 
+                // You can check the implementation below.
+                //IconComponent = HomeIconWithBadge; 
+              } else if (routeName === 'Settings') {
+                iconName = `ios-options`;
+              }
+      
+              // You can return any component that you like here!
+              return <IconComponent name={iconName} size={25} color={tintColor} />;
+            },
+          }),
+          tabBarOptions: {
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray',
+          },
+        }, 
+        {
           tabBarPosition: "bottom"
         })
     },
